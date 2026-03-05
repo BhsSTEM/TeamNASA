a7e1954f82962aab4dbeaa1c0358a56c1d6333d2
Tristan Mettee <tmx3nx@gmail.com>
2026-03-05 14:19:59 -0600

    Gave the register transfer button functionality, even if said functionality is not functional.

--- app/src/main/java/com/example/nasa_taskmaster/Login_Page_V2_Fresh.java
+++ app/src/main/java/com/example/nasa_taskmaster/Login_Page_V2_Fresh.java
@@ -1,6 +1,7 @@
 package com.example.nasa_taskmaster;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class Login_Page_V2_Fresh extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void opRegPg(View view) {
+        System.out.println("Hello World");
     }
 }
