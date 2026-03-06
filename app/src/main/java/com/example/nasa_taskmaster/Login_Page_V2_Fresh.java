a31d97aa0cc2df4a8f5ea7fc102f953a0f23b6ee
Tristan Mettee <tmx3nx@gmail.com>
2026-03-06 15:27:43 -0600

    Added login functions!!!!!!!!!!!!!!

--- app/src/main/java/com/example/nasa_taskmaster/Login_Page_V2_Fresh.java
+++ app/src/main/java/com/example/nasa_taskmaster/Login_Page_V2_Fresh.java
@@ -1,16 +1,27 @@
 package com.example.nasa_taskmaster;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login_Page_V2_Fresh extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import android.widget.Toast;
 
+public class Login_Page_V2_Fresh extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +34,40 @@ public class Login_Page_V2_Fresh extends AppCompatActivity {
         });
     }
 
-    public void opRegPg(View view) {
-        System.out.println("Hello World");
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
     }
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "createUserWithEmail:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                Toast.makeText(Login_Page_V2_Fresh.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        }
+    });
+
+
+
 }
+
+    private void updateUI(FirebaseUser user) {
+
+    }
+    }
