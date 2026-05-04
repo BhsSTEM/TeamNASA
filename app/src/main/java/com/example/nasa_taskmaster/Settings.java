02436c0611b63767f11dcd1024c16e3f170d5a41
Tristan Mettee <tmx3nx@gmail.com>
2026-05-04 15:09:34 -0500

    0.4.7.1 - A few fixes

--- app/src/main/java/com/example/nasa_taskmaster/Settings.java
+++ app/src/main/java/com/example/nasa_taskmaster/Settings.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class Settings extends AppCompatActivity {
@@ -12,16 +15,19 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.settings, new SettingsFragment())
-                    .commit();
-        }
+        //if (savedInstanceState == null) {
+        //    getSupportFragmentManager()
+        //            .beginTransaction()
+        //            .replace(R.id.settings, new SettingsFragment())
+        //            .commit();
+        //}
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.navigationBar4, new Navigation_Bar())
+                .commit();
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
